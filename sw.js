c25c62af8aca4a319ec7df0dcb14c1524c8bd356
h37630796-design <h37630796@gmail.com>
2026-06-30 17:13:20 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "colordock-v12";
+const CACHE = "colordock-v13";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", event => {
