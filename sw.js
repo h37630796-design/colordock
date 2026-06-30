0c4cca45190bd1208ab8e44384fa55f51e4191b9
h37630796-design <h37630796@gmail.com>
2026-06-30 17:20:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE = "colordock-v13";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
+const CACHE = "colordock-v14";
+const ASSETS = ["./", "index.html", "styles.css?v=14", "app.js?v=14", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
